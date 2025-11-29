46a4077eba3b2dd97103aca028e7ec40a09cc035
Nastya <mail@yuhimenko-anastasia.ru>
2025-11-29 13:25:15 +0300

    Initial Commit

---
+++ NoteManager/NoteManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteManagerApp.swift
+//  NoteManager
+//
+//  Created by Anastasia on 29.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteManagerTests/NoteManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteManagerTests.swift
+//  NoteManagerTests
+//
+//  Created by Anastasia on 29.11.2025.
+//
+
+import Testing
+@testable import NoteManager
+
+struct NoteManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
